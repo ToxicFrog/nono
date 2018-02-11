72e2c43f8b2823b6c9302a26431ce2c701b28ed7
Ben Kelly <bk@ancilla.ca>
2018-02-10 22:43:37 -0500

    Clean up and comment hints somewhat

--- src/nono/hints.clj
+++ src/nono/hints.clj
@@ -10,48 +10,54 @@
   (LinearLayout/createLayoutData LinearLayout$Alignment/End))
 (def fill-alignment
   (LinearLayout/createLayoutData LinearLayout$Alignment/Fill))
+(defn- GridLayoutData [expand-h expand-v]
+  (GridLayout/createLayoutData
+    GridLayout$Alignment/FILL GridLayout$Alignment/FILL
+    expand-h expand-v))
 
-(defn- BottomJustifiedColumn [children]
-  (let [panel (doto (Panel.)
-                (.setLayoutManager (lgui/dense-grid 1)))
-        ]
-    (.addComponent panel (Label. "")
-                   (GridLayout/createLayoutData
-                     GridLayout$Alignment/FILL GridLayout$Alignment/FILL
-                     true true))
-    (->> children
-         (map #(.addComponent panel %1
-                              (GridLayout/createHorizontallyEndAlignedLayoutData 1)))
-         dorun)
-    panel))
+(defn- FillSoutheastGravityLinearGrid
+  "Given a BottomJustifiedColumn or RightJustifiedRow, add the necessary padding component to produce the bottom/right justification, then add all the actual children and return the panel."
+  [panel children]
+  (.addComponent panel (Label. "") (GridLayoutData true true))
+  (->> children
+       (map #(.addComponent panel %1 (GridLayoutData false false)))
+       dorun)
+  panel)
+
+(defn- BottomJustifiedColumn
+  "Create a Panel containing the specified children that looks like it uses a vertical LinearLayout, except with the children aligned to the bottom of the Panel."
+  [children]
+  (FillSoutheastGravityLinearGrid
+    (doto (Panel.) (.setLayoutManager (lgui/dense-grid 1)))
+    children))
 
 (defn- RightJustifiedRow [spacing children]
-  (let [panel (doto (Panel.)
-                (.setLayoutManager (-> children count inc lgui/dense-grid (.setHorizontalSpacing spacing))))
-        ]
-    (.addComponent panel (Label. "")
-                   (GridLayout/createLayoutData
-                     GridLayout$Alignment/FILL GridLayout$Alignment/FILL
-                     true true))
-    (->> children
-         (map #(.addComponent panel %1
-                              (GridLayout/createLayoutData
-                                GridLayout$Alignment/FILL GridLayout$Alignment/FILL
-                                false false)))
-         dorun)
-    panel))
+  "Create a Panel containing the specified children that looks like it uses a horizontal LinearLayout, except with the children aligned to the right of the Panel."
+  (FillSoutheastGravityLinearGrid
+    (doto (Panel.)
+      (.setLayoutManager
+        (-> children count inc
+            lgui/dense-grid
+            (.setHorizontalSpacing spacing))))
+    children))
 
-(defn- linear-panel [dir]
+(defn- linear-panel
+  "Return a Panel with a LinearLayout in the specified direction."
+  [dir]
   (doto (Panel.)
     (.setLayoutManager
       (doto (LinearLayout. dir) (.setSpacing 0)))))
 
-(defn- set-width [w widget]
+(defn- set-width
+  "Set the width for the given widget."
+  [w widget]
   (doto widget
     (.setPreferredSize
       (-> widget (.getPreferredSize) (.withColumns w)))))
 
-(defn- create-hints [dir hints]
+(defn- HintPanel
+  "Create a panel of hints oriented in the given direction. Each hint is assumed to be a Panel in its own right. Every even-numbered hint is given a lighter background to make line following easier."
+  [dir hints]
   (let [panel (linear-panel dir)
         ]
     (->> (rest hints)
@@ -61,22 +67,28 @@
     (lgui/add-components panel hints fill-alignment)
     ))
 
-(defn row-hint [row]
+(defn- row-hint
+  "Turn the hints for a single row into a panel containing them with 1em spacing between them."
+  [row]
   (->> row
        (map #(Label. (str %1)))
        (RightJustifiedRow 1)))
 
-(defn row-hints [rows]
-  "Create the hints for the rows."
-  (create-hints Direction/VERTICAL
-                (map row-hint rows)))
+(defn row-hints
+  "Given the row hints (vec of vec of ints), return a vertical panel containing them."
+  [rows]
+  (HintPanel Direction/VERTICAL
+             (map row-hint rows)))
 
-(defn col-hint [col]
+(defn- col-hint
+  "Turn the hints for a single column into a panel containing them, 2em wide, bottom justified."
+  [col]
   (->> col
        (map #(Label. (format "%2d" %1)))
-       ;(map #(set-width 2 %1))
        (BottomJustifiedColumn)))
 
-(defn col-hints [cols]
-  (create-hints Direction/HORIZONTAL
-                (map col-hint cols)))
+(defn col-hints
+  "Given the column hints (vec of vec of ints), return a horizontal panel containing them."
+  [cols]
+  (HintPanel Direction/HORIZONTAL
+             (map col-hint cols)))
