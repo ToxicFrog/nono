c6790f696906753f09233ac882aa4fcd2b08eb27
Ben Kelly <bk@ancilla.ca>
2018-02-13 17:42:26 -0500

    Add Expound for better spec errors

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [com.googlecode.lanterna/lanterna "3.0.0"]
                  [net.mikera/core.matrix "0.62.0"]
+                 [expound "0.5.0"]
                  [prismatic/schema "1.1.7"]]
   ; :core.typed {:check [ttymor.core]}
   :main ^:skip-aot nono.core
--- src/nono/core.clj
+++ src/nono/core.clj
@@ -4,9 +4,12 @@
             [nono.nonogram :as nonogram]
             [nono.gui :as gui]
             [nono.game :as game]
+            [clojure.spec.alpha :as spec]
+            [expound.alpha :as expound]
             [schema.core :as s]))
 
 (s/set-fn-validation! true)
+(set! spec/*explain-out* expound/printer)
 
 (defn -main [filename & args]
   (let [game (-> (nonogram/load filename) (game/create-state))]
