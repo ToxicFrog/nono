eec335028d2863c26ea0e8d5d1485e095bb81927
Ben Kelly <bk@ancilla.ca>
2018-02-17 09:17:42 -0500

    Add the first few keybindings
    Enter remains the same, but space now fills a cell, x clears it, and z
    resets it to unknown state.

--- src/nono/lanterna_gui.clj
+++ src/nono/lanterna_gui.clj
@@ -4,7 +4,7 @@
             [lanterna.settings :refer [Colour Theme]]
             [schema.core :as s :refer [def defn]]
             [clojure.core.matrix :as mx])
-  (:import (com.googlecode.lanterna.gui2 Button Button$FlatButtonRenderer Label)
+  (:import (com.googlecode.lanterna.gui2 Button Button$FlatButtonRenderer Label Interactable$Result)
            ))
 
 ; TODO: move to lanterna.widgets
@@ -37,13 +37,33 @@
                 :full :empty
                 :empty :full})
 
+(defmulti press-button
+  (fn dispatcher [keystroke & args] (.getCharacter keystroke)))
+(defmethod press-button \newline [_ game row col]
+  (game/update-cell! game row col next-tile)
+  true)
+(defmethod press-button \space [_ game row col]
+  (game/set-cell! game row col :full)
+  true)
+(defmethod press-button \x [_ game row col]
+  (game/set-cell! game row col :empty)
+  true)
+(defmethod press-button \z [_ game row col]
+  (game/set-cell! game row col :???)
+  true)
+(defmethod press-button :default [_ game row col]
+  false)
+
 (defn CellButton :- s/Any
   "Create a button associated with a single cell in the grid."
   [game :- game/GameAtom, [row col] :- [s/Int]]
-  (let [labeler (fn [] (tiles (game/get-cell game row col)))
-        handler (partial game/update-cell! game row col next-tile)]
-    (proxy [Button] [(labeler) handler]
+  (let [labeler (fn [] (tiles (game/get-cell game row col)))]
+    (proxy [Button] [(labeler)]
       (createDefaultRenderer [] (Button$FlatButtonRenderer.))
       (getLabel [] (labeler))
+      (handleKeyStroke [keystroke]
+                       (if (press-button keystroke game row col)
+                         Interactable$Result/HANDLED
+                         (proxy-super-cls Button handleKeyStroke keystroke)))
       (afterEnterFocus [how from]
                        (game/set-position! game row col)))))
