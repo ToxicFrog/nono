6402e75c9ce4a4e385c0e3d135fcaafa7c47db9b
Ben Kelly <bk@ancilla.ca>
2018-02-23 20:13:42 -0500

    Update test, such as it is

--- test/nono/nonogram_test.clj
+++ test/nono/nonogram_test.clj
@@ -3,25 +3,28 @@
             [nono.nonogram :refer :all]))
 
 (def test-pattern
-  (str
-    "test pattern\n"
-    "#.#\n"
-    ".#.\n"
-    "###\n"
-    "..#\n"))
+  "{
+  \"title\": \"test pattern\",
+  \"columns\": [[1,1],[2],[1,2]],
+  \"rows\": [[1,1],[1],[3],[1]]
+  }")
 
 (def test-ngram
   {:title "test pattern"
    :width 3
    :height 4
-   :grid [[:full :empty :full]
-          [:empty :full :empty]
-          [:full :full :full]
-          [:empty :empty :full]]
+   :grid [[:??? :??? :???]
+          [:??? :??? :???]
+          [:??? :??? :???]
+          [:??? :??? :???]]
+   ; :grid [[:full :empty :full]
+   ;        [:empty :full :empty]
+   ;        [:full :full :full]
+   ;        [:empty :empty :full]]
    :hints {:col [[1 1] [2] [1 2]]
            :row [[1 1] [1] [3] [1]]}})
 
-(deftest test-str->nonogram
-  (testing "Loading nonogram from string"
+(deftest test-json->nonogram
+  (testing "Loading nonogram from JSON"
     (is (= test-ngram
-           (str->nonogram test-pattern)))))
+           (from-json test-pattern)))))
